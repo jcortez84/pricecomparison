c90098c9276f1fb8bdb7ccb06343446399889321
Peter Ncube <pmn@outlook.com>
2019-03-17 00:22:55 +0000

    Refix of the Product Codes Table

--- database/migrations/2019_01_29_230045_create_product_codes_table.php
+++ database/migrations/2019_01_29_230045_create_product_codes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMatchesTable extends Migration
+class CreateProductCodesTable extends Migration
 {
     /**
      * Run the migrations.
