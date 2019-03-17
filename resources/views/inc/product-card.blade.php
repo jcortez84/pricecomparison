3a9acd88b9c5048aeff3b9e80c14101fbf552286
Peter Ncube <pmn@outlook.com>
2019-03-17 16:25:35 +0000

    display merchant product images

--- resources/views/inc/product-card.blade.php
+++ resources/views/inc/product-card.blade.php
@@ -1,6 +1,12 @@
 <a  class="text-muted" :href="'/compare/'+product.slug+'/prices'">
     <div class="card w-100" >
-        <img v-if="product.images" :src="'/'+product.images.path" class="card-img-top" :alt="product.title" id="'prod_image_'+product.id" >
+        
+        @if(Request::is('retailer/*'))
+            <img v-if="product.images" :src="'/'+product.images[0].path" class="card-img-top" :alt="product.title" id="'prod_image_'+product.id" >
+            
+        @else
+            <img v-if="product.images" :src="'/'+product.images.path" class="card-img-top" :alt="product.title" id="'prod_image_'+product.id" >
+        @endif
         <img v-else src="https://via.placeholder.com/200" class="card-img-top" alt="no-image">
         <div class="card-body">
             <h6 class="lead"> <small>@{{product.title.substring(0,40)+".."}}</small> </h6>
