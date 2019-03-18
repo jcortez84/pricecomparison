110e1f42ff3f63267eae80b23bff14b53bd2dee5
Peter Ncube <pmn@outlook.com>
2019-03-18 22:03:42 +0000

    Error checking pages template

--- resources/views/shopfront/pages/show.blade.php
+++ resources/views/shopfront/pages/show.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
-@section('title', $page->title)
+@if($page)
+        @section('title', $page->title)
+@endif
 @section('content')
 <div class="container">
 <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a class="text-dark" href="/">Home</a></li>
+                @if($page)
                 <li class="breadcrumb-item active" aria-current="page">{{$page->title}}</li>
+                @endif
         </ol>
 </nav>
+@if($page)
         <h1 class="h2 text-success">{{$page->title}}</h1>
         <div>
                 {!!$page->body!!}
         </div>
+@endif
 </div>
 @endsection
