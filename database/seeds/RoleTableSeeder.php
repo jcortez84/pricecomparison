935d57ee090efed221f34e9b16d187092a6beba7
Peter Ncube <pmn@outlook.com>
2019-03-21 21:57:53 +0000

    Roles tables

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $role_employee = new Role();
+      $role_employee->name = 'manager';
+      $role_employee->description = 'A Manager User';
+      $role_employee->save();
+      $role_manager = new Role();
+      $role_manager->name = 'employee';
+      $role_manager->description = 'An Employee User';
+      $role_manager->save();
+      $role_employee = new Role();
+      $role_employee->name = 'customer';
+      $role_employee->description = 'A Manager User';
+      $role_employee->save();
+      $role_manager = new Role();
+      $role_manager->name = 'merchant';
+      $role_manager->description = 'An Employee User';
+      $role_manager->save();
+    }
+}
