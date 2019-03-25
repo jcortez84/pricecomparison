14006debe9efc880e7ff3ceaaa36087875720dc1
Peter Ncube <pmn@outlook.com>
2019-03-25 20:30:08 +0000

    aa

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -66,27 +66,6 @@ class UsersController extends Controller
      */
     public function edit($id)
     {
-        $role_employee = new Role();
-        $role_employee->name = 'manager';
-        $role_employee->description = 'The admin has superuser privilege';
-        $role_employee->save();
-        $role_manager = new Role();
-        $role_manager->name = 'employee';
-        $role_manager->description = 'An Employee can access the admin panel';
-        $role_manager->save();
-        $role_employee = new Role();
-        $role_employee->name = 'customer';
-        $role_employee->description = 'A Customer has access to their own suscriptions';
-        $role_employee->save();
-        $role_manager = new Role();
-        $role_manager->name = 'merchant';
-        $role_manager->description = 'A merchant can add a shop for products listing';
-        $role_manager->save();
-  
-        $manager = new RoleUser();
-        $manager->role_id = 1;
-        $manager->user_id = 1;
-        $manager->save();
         $users = User::find($id);
         $roles = Role::all()->pluck('name', 'id');
         return view('admin.users.edit')->with(compact('users', 'roles'));
