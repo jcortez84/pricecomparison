f0340097bfafef629efce0eedec78a4094934c02
Peter Ncube <pmn@outlook.com>
2019-03-25 20:28:22 +0000

    sort out the account permissions

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -13,19 +13,19 @@ class RoleTableSeeder extends Seeder
     {
       $role_employee = new Role();
       $role_employee->name = 'manager';
-      $role_employee->description = 'A Manager User';
+      $role_employee->description = 'The admin has superuser privilege';
       $role_employee->save();
       $role_manager = new Role();
       $role_manager->name = 'employee';
-      $role_manager->description = 'An Employee User';
+      $role_manager->description = 'An Employee can access the admin panel';
       $role_manager->save();
       $role_employee = new Role();
       $role_employee->name = 'customer';
-      $role_employee->description = 'A Manager User';
+      $role_employee->description = 'A Customer has access to their own suscriptions';
       $role_employee->save();
       $role_manager = new Role();
       $role_manager->name = 'merchant';
-      $role_manager->description = 'An Employee User';
+      $role_manager->description = 'A merchant can add a shop for products listing';
       $role_manager->save();
 
       $manager = new RoleUser();
