c9d8a0090d5c9f730218955eea09dcb471f2aca8
Peter Ncube <pmn@outlook.com>
2019-03-26 13:17:01 +0000

    Change the products table

--- database/migrations/2019_03_26_131520_change_products_table.php
+++ database/migrations/2019_03_26_131520_change_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class ChangeProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,7 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
+        $this->down();
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('category_id');
