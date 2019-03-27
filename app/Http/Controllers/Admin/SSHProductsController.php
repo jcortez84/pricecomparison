3b43e2cb53a293f328f753ed234ee2534914ddd3
Peter Ncube <pmn@outlook.com>
2019-03-27 20:03:34 +0000

    ska

--- app/Http/Controllers/Admin/SSHProductsController.php
+++ app/Http/Controllers/Admin/SSHProductsController.php
@@ -264,7 +264,7 @@ class SSHProductsController extends Controller
     public function runAll()
     {
         $feeds = Datafeed::all();
-        dd($feeds);
+        //dd($feeds);
         foreach($feeds as $feed){
             echo $feed->id;
         }
