c8acad755ad8554c336d170441c8235d5368652c
Peter Ncube <pmn@outlook.com>
2019-03-27 07:51:19 +0000

    dd

--- app/Http/Controllers/Admin/SSHProductsController.php
+++ app/Http/Controllers/Admin/SSHProductsController.php
@@ -264,7 +264,7 @@ class SSHProductsController extends Controller
     public function runAll()
     {
         $feeds = Datafeed::all();
-
+        dd($feed);
         foreach($feeds as $feed){
             echo $feed->id;
         }
