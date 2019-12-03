1396df4213f52b008b2745bc9101bf3c0dd08767
wizdom75 <pmn@outlook.com>
2019-12-03 11:23:48 +0000

    Correctected category listing

--- resources/views/inc/menu.blade.php
+++ resources/views/inc/menu.blade.php
@@ -3,20 +3,27 @@
         <div class="row menu-row-margin">
             <div class="col-12 menu-col-padding">
                 <nav class="navbar navbar-expand-md navbar-light bg-teal nav-menu p-0">
-                   <div class="collapse navbar-collapse">
+                    <div class="collapse navbar-collapse">
                         <ul class="navbar-nav nav-menu-ul">
-                            @foreach(App\Category::where('is_featured', 1)->take(9)->get() as $category )
+                            @foreach(App\Category::where('is_featured', '1')->take(9)->get() as $category )
                             <li class="nav-menu-btn">
-                                <a class="btn-menu text-uppercase nav-a" id="dropdown{{$category->id}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><small>{{$category->title}}</small></a>
-                                <div class="dropdown-menu megamenu rounded-0" aria-labelledby="dropdown{{$category->id}}">
+                                <a class="btn-menu text-uppercase nav-a" id="dropdown{{$category->id}}"
+                                    data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false"><small>{{$category->title}}</small></a>
+                                <div class="dropdown-menu megamenu rounded-0"
+                                    aria-labelledby="dropdown{{$category->id}}">
                                     <div class="row">
                                         @foreach (App\Category::where('parent_id', $category->id)->get() as $child)
-                                            <div class="col-md-4 col-lg-3">
-                                                <a  class="dropdown-item text-uppercase text-teal" href="/c/{{$child->slug}}"><span class="text-teal">{{$child->title}}</span></a>
-                                                    @foreach (App\Category::where('parent_id', $child->id)->get() as $grand_child)
-                                                    <a class="dropdown-item nav-link" href="/c/{{$grand_child->slug}}"><span class="text-pink ml-3">--- </span>{{ $grand_child->title }}</a>
-                                                    @endforeach
-                                            </div>
+                                        <div class="col-md-4 col-lg-3">
+                                            <a class="dropdown-item text-uppercase text-teal"
+                                                href="/c/{{$child->slug}}"><span
+                                                    class="text-teal">{{$child->title}}</span></a>
+                                            @foreach (App\Category::where('parent_id', $child->id)->get() as
+                                            $grand_child)
+                                            <a class="dropdown-item nav-link" href="/c/{{$grand_child->slug}}"><span
+                                                    class="text-pink ml-3">--- </span>{{ $grand_child->title }}</a>
+                                            @endforeach
+                                        </div>
                                         @endforeach
                                     </div>
                                 </div>
@@ -27,5 +34,5 @@
                 </nav>
             </div>
         </div>
-    </div>                 
+    </div>
 </div>
