c4d071f7befce6e071945f48436b80e6e235a0d6
wizdom75 <pmn@outlook.com>
2019-12-04 23:37:53 +0000

    Finalised color of image placeholder

--- resources/views/inc/product-card.blade.php
+++ resources/views/inc/product-card.blade.php
@@ -3,11 +3,11 @@
 
         @if(Request::is('retailer/*'))
         <img :src="'/'+product.images[0].path" class="card-img-top rounded-0" :alt="product.title"
-            id="'prod_image_'+product.id" onError=" this.src='https://via.placeholder.com/200x150/CC0?text=No+image'">
+            id="'prod_image_'+product.id" onError=" this.src='https://via.placeholder.com/200x150/4Cb?text=No+image'">
 
         @else
         <img :src="'/'+product.images.path" class="card-img-top rounded-0" :alt="product.title"
-            id="'prod_image_'+product.id" onError="this.src = 'https://via.placeholder.com/200x150/CC0?text=No+image'">
+            id="'prod_image_'+product.id" onError="this.src = 'https://via.placeholder.com/200x150/4Cb?text=No+image'">
         @endif
         <div class="card-body">
             <h6> <small>@{{product.title.substring(0,100)+" ..."}}</small> </h6>
